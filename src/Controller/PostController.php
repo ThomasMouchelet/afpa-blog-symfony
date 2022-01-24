6fa52ed8ccb3d5495b7c4fc4420c98a937bbc314
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-01-24 15:16:59 +0100

    delete post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,11 +23,16 @@ class PostController extends AbstractController
     }
 
     #[Route('/posts/{id}/delete', name: "post.delete")]
-    public function delete(){
+    public function delete(Request $request, PostRepository $postRepo, $id, Post $post, ManagerRegistry $doctrine){
         // 1 : Transmettre l'id dans twig
         // 2 : Récupérer le post en fonction de l'id
+        // $id = $request->get('id');
+        // $post = $postRepo->findOneBy(['id' => $id]);
         // 3 : delete with repository
+        $em = $doctrine->getManager();
+        $em->remove($post);
+        $em->flush();
         // 4 : redirect to route index
-        dd('Delete');
+        return $this->redirectToRoute('post.index');
     }
 }
